8a2b8283d2ff6b0d89e8cc4013ad780fb3ec4a35
bpsgreat23 <96990602+bpsgreat23@users.noreply.github.com>
2024-10-04 22:21:19 +0530

    second commit

--- src/SolanaWallet.jsx
+++ src/SolanaWallet.jsx
@@ -21,7 +21,7 @@ export function SolanaWallet({ mnemonic }) {
             Add SOL wallet
         </button>
         {publicKeys.map(p => <div>
-            {p.toBase58()}
+            {p.toBase58()};
         </div>)}
     </div>
 }
